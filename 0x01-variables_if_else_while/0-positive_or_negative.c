758c0c1da59b996a8ac9d6d5ad4640bb23fd20d2
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-08 22:11:27 +0100

    New git update in low level programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  * 
  * Return:Always 0 (success/correct)
@@ -11,17 +11,17 @@
   int n; 
 srand(time(0)); 
 n = rand() - RAND_MAX / 2; 
-if(n==0)
+if (n==0)
 {
-	printf("%i is zero\n", n);
+	printf("%d is zero\n", n);
 }
-else if(n < 0)
+else if (n < 0)
 {
-	printf("%i is negative\n", n);
+	printf("%d is negative\n", n);
 }
 else
 {
-	printf("%i is positive\n", n);
+	printf("%d is positive\n", n);
 }
 return (0); 
  }
