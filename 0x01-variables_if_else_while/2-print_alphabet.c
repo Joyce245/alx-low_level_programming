b0a91d70674be20c68c307c8cda15873d6d1326e
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-09 00:52:44 +0100

    New git update in low level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 /** 
- * main - program that prints the alphabet in lowercase, followed by a new line 
- * You can only use the putchar 
- * Return: 0 
-*/ 
-	int main(void)
- {
-	 int i = 'a';
-	 while (i <= 'z')
-	 {
+* main - program that prints the alphabet 
+* You can only use the putchar 
+* Return: 0 
+*/
+int main(void)
+{
+int i = 'a';
+while (i <= 'z')
+{
  putchar(i);
  i += 1;
  } 
