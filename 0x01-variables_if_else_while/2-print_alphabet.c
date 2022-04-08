22710253b303e92e1095f5b10cfcc4a54f236808
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-09 00:11:52 +0100

    New git update in low level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,13 @@
  * You can only use the putchar 
  * Return: 0 
 */ 
-  
- int main(void) 
- { 
- int l = 'a'; 
- while (l <= 'z') 
- { 
- putchar(l); 
- l += 1; 
+	int main(void)
+ {
+	 int 1 = 'a';
+	 while (1 <= 'z')
+	 {
+ putchar(1);
+ 1 += 1;
  } 
  putchar('\n'); 
  return (0);
