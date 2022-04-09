3feeef6b92a58af226aa234b08eef830afb3f767
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-09 01:03:42 +0100

    New git update in low level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
-* main - Entry point 
-* Return: 0 
+* main - Entry point
+* * Return: 0 
 */
 int main(void)
 {
-int i = 'a';
+	int i = 'a';
 while (i <= 'z')
 {
 putchar(i);
 i += 1;
-} 
+}
 putchar('\n'); 
 return (0);
 }
