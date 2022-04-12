6dae852c93a0d2d5a405a78aab3a14b3aaaaad02
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-12 16:05:14 +0100

    New nested loop update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: Always 0
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
