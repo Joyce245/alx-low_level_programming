4c17d876b589f5138223ef57f21856e2eff9abaa
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-04-15 03:00:12 +0100

    New update for 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,30 +1,30 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * print_triangle - print triangle of #
+ *
+ * @size: triangle size
  */
 void print_triangle(int size)
 {
-if (size <= 0)
-{
-_putchar('\n');
-} else
-{
 int i, j;
-
-for (i = 1; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-for (j = i; j < size; j++)
+for (j = 1; j <= size; j++)
 {
-_putchar(' ');
-}
-
-for (j = 1; j <= i; j++)
+if (j >= (size - i))
 {
 _putchar('#');
 }
+else
+{
+_putchar(' ');
+}
+}
 _putchar('\n');
 }
+if (size <= 0)
+{
+_putchar('\n');
 }
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,24 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer n
- *@n: integer to be printed
+ * print_number - print integer
+ *
+ * @n: number to print
  */
 void print_number(int n)
 {
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+unsigned int num = 0;
+if (n < 0)
+{
+num = -n;
+_putchar('-');
+}
+else
+{
+num = n;
+}
+if (num / 10)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + 48);
 }
