4a80b0879d1fc08afa29cbd237ddd26778a6808e
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-11-03 02:19:34 +0100

    New update

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,45 @@
+Bytes
+#include "search_algos.h"
+
+/**
+ * interpolation_search - search of segmets in each middle
+ * @array: The sort array
+ * @size: Size of the array
+ * @value: Value to search into the array
+ * Return: The index of value otherwise -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int pos;
+
+	if (array == NULL)
+		return (-1);
+
+	while (array[high] != array[low])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			     * (value - array[low]));
+
+		if (pos > (int)size)
+		{
+			printf("Value checked array[%d] is out of range\n", pos);
+			return (-1);
+		}
+
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
+		else
+			return (pos);
+	}
+
+	if (value == array[low])
+		return (low);
+	else
+		return (-1);
+}
