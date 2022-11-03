fbf0f615242afa906604e7a8ee2c951d1d08aa98
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-11-03 02:23:33 +0100

    New update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,19 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+/*LINKED LIST*/
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*LINKED ALGORITHMS*/
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 
 
 #endif /* SEARCH_ALGOS_H */
